91defba42e96461156ecf816f953dadc60da9a79
Mohammed Habeb <mohammed0habeb@gmail.com>
2022-06-25 13:22:07 +0100

    backend error handling setup

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./db/connect.js";
-dotenv.config();
 const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+import "express-async-errors";
+
+//db and authenticateUser
+import connectDB from "./db/connect.js";
+
+//routers
+import authRouter from "./routes/authRoutes.js";
+import jobsRouter from "./routes/jobsRoutes.js";
 
 //middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 
+// make json data available to us in post controllers
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  throw new Error("error");
   res.send("Welcome!");
 });
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
