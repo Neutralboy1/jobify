773f8470bc262c6c3ea3569714a7ebe7ffd38995
Mohammed Habeb <mohammed0habeb@gmail.com>
2022-06-27 22:01:15 +0100

    connected frontend and backend using concurrently. used proxy to access server url from frontend

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Welcome!");
 });
+app.get("/api/v1", (req, res) => {
+  res.send({ msg: "API" });
+});
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", jobsRouter);
