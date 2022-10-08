61594d181e953d6af51e4f38c3b8b7f2f44b103c
Mohammed Habeb <mohammed0habeb@gmail.com>
2022-10-08 13:19:32 +0100

    added stats components to stats page

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -28,6 +28,8 @@ import {
   EDIT_JOB_BEGIN,
   EDIT_JOB_SUCCESS,
   EDIT_JOB_ERROR,
+  SHOW_STATS_BEGIN,
+  SHOW_STATS_SUCCESS,
 } from "./actions";
 
 const token = localStorage.getItem("token");
@@ -56,6 +58,8 @@ const initialState = {
   totalJobs: 0,
   numOfPages: 1,
   page: 1,
+  stats: {},
+  monthlyApplications: [],
 };
 
 const AppContext = React.createContext();
@@ -272,7 +276,22 @@ const AppProvider = ({ children }) => {
       //logoutUser();
     }
   };
-
+  const showStats = async () => {
+    dispatch({ type: SHOW_STATS_BEGIN });
+    try {
+      const { data } = await authFetch("/jobs/stats");
+      dispatch({
+        type: SHOW_STATS_SUCCESS,
+        payload: {
+          stats: data.defaultStats,
+          monthlyApplications: data.monthlyApplications,
+        },
+      });
+    } catch (error) {
+      console.log(error.response);
+      //logoutUser()
+    }
+  };
   return (
     <AppContext.Provider
       value={{
@@ -290,6 +309,7 @@ const AppProvider = ({ children }) => {
         setEditJob,
         deleteJob,
         editJob,
+        showStats,
       }}
     >
       {children}
